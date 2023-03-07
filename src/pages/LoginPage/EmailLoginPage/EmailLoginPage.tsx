be707bd6538c21b79ffa85796d11fbb538077575
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-03-07 18:12:09 +0900

    remove: 불필요하게 사용되는 import문 삭제

--- src/pages/LoginPage/EmailLoginPage/EmailLoginPage.tsx
+++ src/pages/LoginPage/EmailLoginPage/EmailLoginPage.tsx
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import useInput from '../../../hooks/useInput';
 import Button from '../../../components/common/Button/Button';
 import EmailLoginInput from './EmailLoginInput';
